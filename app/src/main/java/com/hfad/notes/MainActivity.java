b7038912be7e85022ceb29de55294cf6e33a14b2
aleks-hd2015 <aleks-hd2015@yandex.ru>
2021-07-10 19:35:27 +0400

    Toolbar реализован

--- app/src/main/java/com/hfad/notes/MainActivity.java
+++ app/src/main/java/com/hfad/notes/MainActivity.java
@@ -1,14 +1,18 @@
 package com.hfad.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
-
-
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,11 +23,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.containertoolbar);
         initButton();
         initListener();
 
-      //  initToolbar();
+        initToolbar();
 
     }
 
@@ -32,6 +36,47 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_favorite:
+                Toast.makeText(MainActivity.this, "Избранные", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(MainActivity.this, "Настройки", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_main:
+                Toast.makeText(MainActivity.this, "Главная", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) search.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
 
     void initButton() {
         addBtn = findViewById(R.id.addBtn);
